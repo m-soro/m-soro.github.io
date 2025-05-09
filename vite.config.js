e2ee32622dc86045884ad0e7c0d1f097b808ff29
Mark Soro <napoleon_soro@rocketmail.com>
2025-05-09 12:29:26 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/m-soro.github.io/",
+  build: {
+    outDir: "docs",
+  },
 });
