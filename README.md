85ce8b1b134cc831ca64cb230e8512c6f9c394b9
Mark Soro <napoleon_soro@rocketmail.com>
2025-05-11 10:47:40 -0400

    Setup GitHub Actions for automated deployment to GitHub Pages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Mark's Software Development Portfolio
 
-Built May 2025 to replace my September 2023 portfolio. 
+![Deploy Status](https://github.com/m-soro/m-soro.github.io/actions/workflows/deploy.yml/badge.svg)
+
+Built May 2025 to replace my September 2023 portfolio.
--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -101,18 +101,14 @@ const ProjectModal = ({ project, onClose }) => {
                 <Slider ref={sliderRef} {...settings}>
                   {project.images.map((img, index) => (
                     <div key={index} className="project-carousel-slide">
-                      <div
-                        className="modal-image-wrapper flex items-center justify-center"
-                        style={{ height: "400px" }}
-                      >
+                      <div className="modal-image-wrapper">
                         <img
                           src={img}
                           alt={`${project.title} - image ${index + 1}`}
-                          className="modal-carousel-image max-h-full max-w-full"
+                          className="modal-carousel-image"
                           style={{
                             objectFit: "contain",
                             objectPosition: "center",
-                            transform: "none",
                           }}
                         />
                       </div>
